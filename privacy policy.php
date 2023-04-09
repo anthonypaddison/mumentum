21284b0caed2010941123b158fc10ac4a18db1ad
anthonypaddison <anthony@paddison.co.uk>
2023-04-09 18:46:48 +0100

    First Commit

---
+++ privacy policy.php
@@ -0,0 +1,42 @@
+<?php include("heading.inc"); ?>
+<br/>
+<br/>
+<h1>Privacy Policy</h1>
+<p>At Mumentum, we value your privacy and are committed to protecting your personal information. This Privacy Policy outlines how we collect, use, disclose, and protect the information you provide to us through the use of our website.</p>
+
+<h2>Collection of Information</h2>
+
+<p>We collect personal information when you sign up for our website or use our services. This information may include your name, email address, and other contact information. We may also collect information about your preferences, interests, and location. We collect this information to provide you with a more personalized and enjoyable experience on our website.</p>
+
+<h2>Use of Information</h2>
+
+<p>We use the information we collect to provide you with our services and improve your user experience. We may use your personal information to contact you about events, news, and other information relevant to mums. We may also use your information to analyze user behavior and improve our website's functionality and content.</p>
+
+<h2>Disclosure of Information</h2>
+
+<p>We may share your personal information with third-party service providers to help us provide our services. We may also share your information with our partners and sponsors to offer you relevant information and discounts. We may disclose your information if required by law or to protect our legal rights.</p>
+
+<h2>Security</h2>
+
+<p>We take reasonable measures to protect your personal information from unauthorized access, disclosure, or modification. However, no security measures can guarantee complete protection of your information.</p>
+
+<h2>Cookies</h2>
+
+<p>We may use cookies and other tracking technologies to collect information about your use of our website. Cookies help us provide you with a more personalized experience and analyze user behavior.</p>
+
+<h2>Third-party Links</h2>
+
+<p>Our website may contain links to third-party websites. We are not responsible for the privacy practices or content of these websites.</p>
+
+<h2>Children's Privacy</h2>
+
+<p>Our website is not intended for children under the age of 13. We do not knowingly collect personal information from children under 13 years of age.</p>
+
+<h2>Changes to this Policy</h2>
+
+<p>We may update this Privacy Policy from time to time. Any changes will be posted on our website. Your continued use of our website constitutes your acceptance of the revised Privacy Policy.</p>
+
+<p>If you have any questions or concerns about our Privacy Policy, please contact us at privacy@mumentum.com.</p>
+<br/>
+<br/>
+<?php include("footer.inc"); ?>
